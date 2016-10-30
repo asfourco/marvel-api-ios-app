8efe74cbefddefc5d4960c8c72e79d1dcf6c80fd
Fadi Asfour <fadi@asfourconsulting.com>
2016-10-30 03:01:12 -0700

    cleanup

--- MarvelAPIDemo/ComicTableViewCell.swift
+++ MarvelAPIDemo/ComicTableViewCell.swift
@@ -25,18 +25,4 @@ class ComicTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
-
-    func getComicImage(url: URL) {
-        print("getComicImage called")
-        Alamofire.download(url.absoluteString).responseData {
-            response in
-
-            print("completion of Alamofire.download")
-            if let data = response.result.value {
-                print("setting image")
-                self.thumbnail.image = UIImage(data: data)
-            }
-        }
-    }
-
 }
